042820834c77ff3b4078f32fc0b33ff552ede26c
adlibe <adrianbergersen@outlook.com>
2024-09-02 15:07:18 +0200

    Initial commit

---
+++ src/main/java/com/DAT250/exp2/PollAppApplication.java
@@ -0,0 +1,13 @@
+package com.DAT250.exp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DAT250/exp2/PollAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DAT250.exp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
