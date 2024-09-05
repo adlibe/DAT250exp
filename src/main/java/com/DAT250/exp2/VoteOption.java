fbfa1ed2eac60f57bb1c8f72293e47a3f3530b51
adlibe <adrianbergersen@outlook.com>
2024-09-05 18:17:52 +0200

    Added getters and setters

--- src/main/java/com/DAT250/exp2/VoteOption.java
+++ src/main/java/com/DAT250/exp2/VoteOption.java
@@ -1,4 +1,23 @@
 package com.DAT250.exp2;
 
 public class VoteOption {
+    private String caption;
+    private int presentationOrder;
+
+    public void setCaption(String caption) {
+        this.caption = caption;
+    }
+
+    public String getCaption() {
+        return caption;
+    }
+
+    public void setPresentationOrder(int presentationOrder) {
+        this.presentationOrder = presentationOrder;
+    }
+
+    public int getPresentationOrder() {
+        return presentationOrder;
+    }
 }
+
