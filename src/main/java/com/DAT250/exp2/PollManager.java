ff28f223e1d093a3beac1e58343e7654bea2a892
adlibe <adrianbergersen@outlook.com>
2024-09-05 18:47:11 +0200

    Added hashmaps for users and polls

--- src/main/java/com/DAT250/exp2/PollManager.java
+++ src/main/java/com/DAT250/exp2/PollManager.java
@@ -1,4 +1,12 @@
 package com.DAT250.exp2;
 
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
 public class PollManager {
+    private Map<String, User> users = new HashMap<>();
+    private Map<String, Poll> polls = new HashMap<>();
 }
