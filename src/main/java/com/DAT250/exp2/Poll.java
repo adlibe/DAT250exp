5f4810508428cacde70bac4dbd0c1b2d5aa34e9d
adlibe <adrianbergersen@outlook.com>
2024-09-05 18:11:41 +0200

    Added getters and setters

--- src/main/java/com/DAT250/exp2/Poll.java
+++ src/main/java/com/DAT250/exp2/Poll.java
@@ -1,4 +1,33 @@
 package com.DAT250.exp2;
 
 public class Poll {
+    private String question;
+    private Instant publishedAt;
+    private Instant validUntil;
+
+    public String getQuestion () {
+        return question;
+    }
+
+    public void setQuestion (String question) {
+        this.question = question;
+    }
+
+    public Instant setPublishedAt () {
+        return publishedAt;
+    }
+
+    public void getPublishedAt (Instant publishedAt) {
+        this.publishedAt = publishedAt;
+    }
+
+    public Instant setValidUntil () {
+        return validUntil;
+    }
+
+    public void getValidUntil (Instant validUntil) {
+        this.validUntil = validUntil;
+    }
 }
+
+
