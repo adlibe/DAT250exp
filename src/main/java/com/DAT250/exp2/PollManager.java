06f93032353d8d3976e175061b5b89e45b0d6581
adlibe <adrianbergersen@outlook.com>
2024-09-05 18:05:27 +0200

    Added getters and setters

--- src/main/java/com/DAT250/exp2/PollManager.java
+++ src/main/java/com/DAT250/exp2/PollManager.java
@@ -1,4 +1,4 @@
 package com.DAT250.exp2;
 
-public class DomainManager {
+public class PollManager {
 }
--- src/main/java/com/DAT250/exp2/User.java
+++ src/main/java/com/DAT250/exp2/User.java
@@ -1,4 +1,24 @@
 package com.DAT250.exp2;
 
 public class User {
+    private String username;
+    private String email;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String username){
+        this.email = email;
+    }
+
+
 }
