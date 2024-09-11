7cadbde971a3de1d4eabede1051aee0b3b4f691a
adlibe <adrianbergersen@outlook.com>
2024-09-11 21:14:56 +0200

    Added auto refresh configs

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,10 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-})
+  server: {
+    watch: {
+      usePolling: true, // Helpful in some environments to watch for file changes
+    },
+    hmr: true,  // Enable Hot Module Replacement
+  },
+});
