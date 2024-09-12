08d6d84272ebd2c77fb9114b1e636d3d8f103b59
adlibe <adrianbergersen@outlook.com>
2024-09-12 23:01:50 +0200

    deployed

--- backend/src/main/java/com/DAT250/exp2/beans/Poll.java
+++ backend/src/main/java/com/DAT250/exp2/beans/Poll.java
@@ -14,6 +14,7 @@ public class Poll {
 
     public Poll() {
         this.pollId = UUID.randomUUID().toString();
+        this.publishedAt = Instant.now();
     }
 
     public String getQuestion () {
