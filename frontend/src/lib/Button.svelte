adf977df2330269ca59fb2ef2445bf621c7cba72
adlibe <adrianbergersen@outlook.com>
2024-09-12 11:58:03 +0200

    Removed premade clutter, and added some simple buttons

---
+++ frontend/src/lib/Button.svelte
@@ -0,0 +1,25 @@
+<script>
+    let showButtons = false;
+
+    function openButtons() {
+        showButtons = true;
+    }
+</script>
+
+<button on:click={openButtons}>Start creating polls!</button>
+
+<!-- Conditionally render new buttons if showButtons is true -->
+{#if showButtons}
+    <div>
+        <button on:click={() => alert('Button 1 clicked')}>Create User</button>
+        <button on:click={() => alert('Button 2 clicked')}>Create Poll</button>
+        <button on:click={() => alert('Button 3 clicked')}>Vote on Poll</button>
+    </div>
+{/if}
+
+<style>
+    /* Optional: Add some margin or styling for the buttons */
+    button {
+        margin: 5px;
+    }
+</style>
